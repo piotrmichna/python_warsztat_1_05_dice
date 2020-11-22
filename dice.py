0af468892262dfd738b293e704e5fff9211d0452
Piotr Michna <pm@piotrmichna.pl>
2020-11-22 20:49:58 +0100

    start definition fun. dice_get_command(str)

---
+++ dice.py
@@ -0,0 +1,16 @@
+from random import randint
+
+
+def dice_get_command(str_command):
+    # return dice=[x,y,'operation',z] or False
+    dice = []
+
+    d_command = str_command.upper()
+    if 'D' not in d_command:
+        return False
+
+
+
+    return dice
+
+
